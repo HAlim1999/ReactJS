bbec5e795df7a191aae6900ce8eba8f423acf1de
Ivan <82239564+HAlim1999@users.noreply.github.com>
2022-07-03 17:37:47 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './componentes/Header';
+import Home from './componentes/Home';
+
+function App() {
+  return (
+    <div className="app">
+    <Header/>
+    <Home/>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCuGJmN-YpnUnAaHwjBeCg9GU_HI90OGDs",
+    authDomain: "proyecto-322ce.firebaseapp.com",
+    projectId: "proyecto-322ce",
+    storageBucket: "proyecto-322ce.appspot.com",
+    messagingSenderId: "961556497839",
+    appId: "1:961556497839:web:5f40965cf4761be8ca5b5a",
+    measurementId: "G-51G53TKVY9"
+  };
